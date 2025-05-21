449c4102d07f5f8b215f062f9207ce6953216dd3
mail.prabin.shrestha@gmail.com <mail.prabin.shrestha@gmail.com>
2025-05-21 18:51:11 -0400

    add gender specific questions

--- app/static/form-renderer.js
+++ app/static/form-renderer.js
@@ -9,17 +9,21 @@ fetch('static/data.json').then(res => res.json()).then(data => {
     switch (item.type) {
       case "boolean":
         formInputs.push(`
-          <li>
+          <li id='q-${item['name']}'>
             <div>
               <label class='font-medium'>${item['question']}</label>
               <div class='flex flex-row space-x-4'>
                 <div>
-                  <input type='radio' id='${item['name']}-yes' name='${item['name']}' value='Yes' />
-                  <label for='${item['name']}-yes'>Yes<label>
+                  <label for='${item['name']}-yes'>
+                    <input type='radio' id='${item['name']}-yes' name='${item['name']}' value='Yes' />
+                    Yes
+                  <label>
                 </div>
                 <div>
-                  <input type='radio' id='${item['name']}-no' name='${item['name']}' value='No' />
-                  <label for='${item['name']}-no'>No<label>
+                  <label for='${item['name']}-no'>
+                    <input type='radio' id='${item['name']}-no' name='${item['name']}' value='No' />
+                    No
+                  <label>
                 </div>
               </div>
             </div>
@@ -32,14 +36,16 @@ fetch('static/data.json').then(res => res.json()).then(data => {
         for (const option of item['options']) {
           options.push(`
             <div>
-              <input type='radio' id='${item['name']}-${option}' name='${item['name']}' value='${option}' />
-              <label for='${item['name']}-${option}'>${option}<label>
+              <label for='${item['name']}-${option}'>
+                <input type='radio' id='${item['name']}-${option}' name='${item['name']}' value='${option}' />
+                ${option}
+              <label>
             </div> 
           `)
         }
 
         formInputs.push(`
-          <li>
+          <li id='q-${item['name']}'>
             <label class='font-medium'>${item['question']}</label>
             <div>
               ${options.join('')} 
@@ -53,14 +59,16 @@ fetch('static/data.json').then(res => res.json()).then(data => {
         for (const option of item['options']) {
           multiselectOptions.push(`
             <div>
-              <input type='checkbox' id='${item['name']}-${option}' name='${item['name']}' value='${option}' />
-              <label for='${item['name']}-${option}'>${option}<label>
+              <label for='${item['name']}-${option}'>
+                <input type='checkbox' id='${item['name']}-${option}' name='${item['name']}' value='${option}' />
+                ${option}
+              <label>
             </div> 
           `)
         }
 
         formInputs.push(`
-          <li>
+          <li id='q-${item['name']}'>
             <label class='font-medium'>${item['question']}</label>
             <div>
               ${multiselectOptions.join('')} 
@@ -71,7 +79,7 @@ fetch('static/data.json').then(res => res.json()).then(data => {
 
       default:
         formInputs.push(`
-          <li>
+          <li id='q-${item['name']}'>
             <div class='grid'>
               <label class='font-medium'>${item['question']}</label>
               <input class='border border-gray-400 bg-gray-50 rounded outline-none px-2 h-8 focus:ring-2 focus:ring-indigo-300' id='${item['name']}' name='${item['name']}' type='${item['type']}' />
@@ -87,5 +95,20 @@ fetch('static/data.json').then(res => res.json()).then(data => {
       ${formInputs.join('')}
     </ol>
   `
+
+  const femaleOnlyInputs = data.filter(v => v.femaleOnly).map(v => v.name)
+  console.log("🚀 ~ fetch ~ femaleOnlyInputs:", femaleOnlyInputs)
+
+  document.getElementById('gender-Male').addEventListener('change', (e) => {
+    femaleOnlyInputs.forEach(input => {
+      document.getElementById(`q-${input}`).style.display = 'none'
+    });
+  })
+
+  document.getElementById('gender-Female').addEventListener('change', (e) => {
+    femaleOnlyInputs.forEach(input => {
+      document.getElementById(`q-${input}`).style.display = 'list-item'
+    });
+  })
 })
 
